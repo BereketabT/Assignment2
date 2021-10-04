e2c21763c7ea3654ec3dc296dba2e507eddfaf34
BereketabT <btessema@princeton.edu>
2021-10-04 15:16:40 -0400

    testing stra.c

--- str.h
+++ str.h
@@ -6,7 +6,7 @@
 size_t Str_getLength(const char input[]);
 char *Str_copy(char dest[], const char source[]);
 char *Str_concat(char dest[], const char source[]);
-int Str_compare(char str1[], char str2[]);
+int Str_compare(const char str1[], const char str2[]);
 char *Str_search (const char str[], const char query[]);
 
 #endif
