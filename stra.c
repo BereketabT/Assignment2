2a63517f50f1ae1bb65c221fe525f1edfff2f75c
BereketabT <btessema@princeton.edu>
2021-10-04 18:06:54 -0400

    testing stra.c

--- stra.c
+++ stra.c
@@ -42,7 +42,7 @@ int Str_compare(const char str1[], const char str2[])
     assert(str2 != NULL);
 
     while (str1[i] == str2[i]) {
-        if((str1[i] != '\0') && (str2[i] != '\0'))
+        if((str1[i] == '\0') && (str2[i] == '\0'))
             return 0;
         i++;
     }
