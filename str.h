1357ef3ca2fc887c3bfd1a4a0f27936de55cc093
BereketabT <btessema@princeton.edu>
2021-10-04 19:50:01 -0400

    testing stra.c

--- str.h
+++ str.h
@@ -3,10 +3,20 @@
 #ifndef STR
 #define STR
 
+/* Returns the length of string input */
 size_t Str_getLength(const char input[]);
+
+/* Copies the string source to dest */
 char *Str_copy(char dest[], const char source[]);
+
+/* Concaticates string source to end of string dest */
 char *Str_concat(char dest[], const char source[]);
+
+/* Compares the strings str1 and str2 alphabetically. If str1 comes before str2, returns negative number.
+   If str1 comes after str2, returns positive number. If the strings are equal, returns 0. */
 int Str_compare(const char str1[], const char str2[]);
+
+/* Searches string haystack for first occurance of string needle and returns pointer to first occurance*/
 char *Str_search (const char haystack[], const char needle[]);
 
 #endif
--- stra.c
+++ stra.c
@@ -1,6 +1,12 @@
 #include <stddef.h>
 #include <assert.h>
 
+size_t Str_getLength(const char input[]);
+char *Str_copy(char dest[], const char source[]);
+char *Str_concat(char dest[], const char source[]);
+int Str_compare(const char str1[], const char str2[]);
+char *Str_search (const char haystack[], const char needle[]);
+
 size_t Str_getLength(const char input[])
 {
     size_t length = 0;
